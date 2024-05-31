39732b8cd2cc1f3b94d7518e4b1b63adcfd0cd7b
KhushiYadav18 <yadavkhushi00045@gmail.com>
2024-05-31 22:13:01 +0530

    Initial commit

---
+++ students/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
